199afd270121bfd4e751eed5e472d2c96af1c9a4
Douglas <38635101+douglasbispop@users.noreply.github.com>
2021-12-27 02:40:22 -0300

    My Portfolio

---
+++ src/data/BlogData.js
@@ -0,0 +1,14 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Por que empreender?",
+        tags:["react","gsap","styled-components"],
+        date:"19 Sep, 2018",
+        imgSrc:"https://miro.medium.com/max/700/1*hmYf8o8_trskHdhhVKww4w@2x.jpeg",
+        link:"https://medium.com/@douglasbispop/por-que-empreender-41194484582"
+    },
+    
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,16 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"My little company website",
+        description:"Created with html+css+js",
+        tags:["html","css","js"],
+        
+        demo:"https://studiomvp.web.app/",
+        github:"https://github.com/douglasbispop/studiomvp-landpage"
+    },
+    
+    
+  
+] 
